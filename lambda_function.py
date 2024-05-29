473e896d79bcafcfd544c15937af0b1479c9aefe
masuumz <object.masum@gmail.com>
2024-05-29 15:06:01 +0600

    ci/cd2 modify3

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,7 @@
-import json
+import pandas as pd
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    return {"statusCode": 200, "body": json.dumps(body)}
+def lambda_handler(event, context):
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.5')
