a2c12c6e3e3383995b6780123000b3960bd27cf5
masuumz <object.masum@gmail.com>
2024-05-29 15:20:45 +0600

    ci/cd2 modify4

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,8 @@
-import pandas as pd
+import json
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.5')
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from github!')
+    }
